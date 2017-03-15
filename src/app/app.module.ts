0c24b41f8d063ea7157e69815c9a1d87962aa1a8
dries11 <daniel.ries@ymail.com>
2017-03-15 14:27:59 -0400

    Initialization

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyB0Bo1Q8njukDF9PCzSsHwtAqpGRBUvxQw",
+    authDomain: "demoionic-df0b4.firebaseapp.com",
+    databaseURL: "https://demoionic-df0b4.firebaseio.com",
+    storageBucket: "demoionic-df0b4.appspot.com",
+    messagingSenderId: "1089273285103"
+}
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
