d7cadda3c36ce5028cd4fe52c04681ec4aab6f3f
dries11 <daniel.ries@ymail.com>
2017-03-22 22:35:03 -0400

    Added all necessary pages

---
+++ src/pages/creategame/creategame.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the Creategame page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-creategame',
+  templateUrl: 'creategame.html'
+})
+export class CreategamePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CreategamePage');
+  }
+
+}
---
+++ src/pages/gameview/gameview-user/gameview-user.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the GameviewUser page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-gameview-user',
+  templateUrl: 'gameview-user.html'
+})
+export class GameviewUserPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad GameviewUserPage');
+  }
+
+}
